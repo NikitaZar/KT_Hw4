07b5cee5b074e3c78fcece52d71467d8aa27607d
Nikita Zarubin <webbizon@ya.ru>
2021-10-23 21:11:57 +0400

    Refactor class VkMoneyTransaction.kt and MoneyAccount.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,17 @@
-fun main(args: Array<String>) {
+fun main() {
     val mastercardAccount = MoneyAccount(AccountTypes.MASTERCARD)
-    VkMoneyTransaction.calcCommission(mastercardAccount, 1000)
-    VkMoneyTransaction.calcCommission(mastercardAccount, 74_001)
+    VkMoneyTransaction.printCommission(mastercardAccount, 1000)
+    VkMoneyTransaction.printCommission(mastercardAccount, 74_001)
 
     val maestroAccount = MoneyAccount(AccountTypes.MAESTRO)
-    VkMoneyTransaction.calcCommission(maestroAccount, 200_000)
+    VkMoneyTransaction.printCommission(maestroAccount, 200_000)
 
     val visaAccount = MoneyAccount(AccountTypes.VISA)
-    VkMoneyTransaction.calcCommission(visaAccount, 1000)
+    VkMoneyTransaction.printCommission(visaAccount, 1000)
 
     val mirAccount = MoneyAccount(AccountTypes.MIR)
-    VkMoneyTransaction.calcCommission(mirAccount, 10_000)
+    VkMoneyTransaction.printCommission(mirAccount, 10_000)
 
     val vkPayAccount = MoneyAccount(AccountTypes.VK_PAY)
-    VkMoneyTransaction.calcCommission(vkPayAccount, 1_000_000)
-
+    VkMoneyTransaction.printCommission(vkPayAccount, 1_000_000)
 }
--- src/main/kotlin/VkMoneyTransaction.kt
+++ src/main/kotlin/VkMoneyTransaction.kt
@@ -1,10 +1,58 @@
 class VkMoneyTransaction {
     companion object {
-        fun calcCommission(srcAccount: MoneyAccount, transactionAmount: Long) {
-            srcAccount.setCurTransaction(transactionAmount)
-            val commission = srcAccount.getCommission()
+
+        private const val MAESTRO_COMMISSION_UNDER_LIMIT_PERCENT = 0.0
+        private const val MAESTRO_COMMISSION_MONTH_LIMIT = 75_000L
+        private const val MAESTRO_COMMISSION_LIMITED_PERCENT = 0.6
+        private const val MAESTRO_COMMISSION_LIMITED_ADD = 20L
+
+        private const val VISA_COMMISSION_PERCENT = 0.75
+        private const val VISA_COMMISSION_MINIMAL = 35L
+
+        private const val KOP_PER_RUB = 1000L
+
+        private fun rubToKop(amount: Long): Long {
+            return amount * KOP_PER_RUB
+        }
+
+        private fun kopToRub(amount: Long): Long {
+            return amount / KOP_PER_RUB
+        }
+
+        private fun calcCommission(srcAccount: MoneyAccount, transactionAmount: Long): Long {
+
+            val accountType = srcAccount.getAccountType()
+            val transactionKop = rubToKop(transactionAmount)
+            val transactionKopVisa = transactionKop * VISA_COMMISSION_PERCENT / 100
+            val visaCommissionMinimalKop = rubToKop(VISA_COMMISSION_MINIMAL)
+            val isLimitedTransaction = srcAccount.getMonthTransactionAmount() > MAESTRO_COMMISSION_MONTH_LIMIT
+
+            val commission = when (accountType) {
+                AccountTypes.MAESTRO, AccountTypes.MASTERCARD ->
+                    if (isLimitedTransaction) {
+                        transactionKop * MAESTRO_COMMISSION_LIMITED_PERCENT / 100 + rubToKop(
+                            MAESTRO_COMMISSION_LIMITED_ADD
+                        )
+                    } else {
+                        transactionKop * MAESTRO_COMMISSION_UNDER_LIMIT_PERCENT / 100
+                    }
+                AccountTypes.VISA, AccountTypes.MIR ->
+                    if (transactionKopVisa < visaCommissionMinimalKop) {
+                        visaCommissionMinimalKop
+                    } else {
+                        transactionKopVisa
+                    }
+                AccountTypes.VK_PAY -> 0L
+            }
+
+            return kopToRub(commission.toLong())
+        }
+
+        fun printCommission(srcAccount: MoneyAccount, transactionAmount: Long) {
+            val commission = calcCommission(srcAccount, transactionAmount)
             println("Перевод с карты ${srcAccount.getAccountType()} на сумму $transactionAmount руб.")
             println("Комиссия по переводу: $commission руб. \n")
         }
+
     }
 }
